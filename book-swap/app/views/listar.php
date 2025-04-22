e2e3d426f68843696330cc6b4c41b79cffd40007
Artur Costa <arturscosta2005@gmail.com>
2025-04-22 19:13:56 -0300

    Um pouco de css

--- book-swap/app/views/listar.php
+++ book-swap/app/views/listar.php
@@ -2,19 +2,26 @@
 <html>
 <head>
     <title>Lista de Livros</title>
+    <link rel="stylesheet" type="text/css" href="/book-swap/public/style.css">
 </head>
 <body>
-    <form action="index.php" method="post">
-        <p>Título</p>
-        <input type="text" name="titulo">
-        <p>Autor</p>
-        <input type="text" name="autor"><br><br>
-        <input type="submit" name="Cadastrar Livro">
-    </form>
-    <h1>Livros Disponíveis</h1>
+    <header>
+        <h1>Book Swap</h1>
+    </header>
+    <div>
+        <h2>Adicione um livro!</h2>
+        <form action="index.php" method="post">
+            <p>Título</p>
+            <input class="input-text" type="text" name="titulo">
+            <p>Autor</p>
+            <input class="input-text" type="text" name="autor"><br><br>
+            <input class="input-submit" type="submit" name="Cadastrar Livro">
+        </form>
+    </div>
+    <h2>Livros Disponíveis</h2>
     <ul>
         <?php foreach ($livros as $livro): ?>
-            <li><?= $livro['titulo'] ?> - <?= $livro['autor'] ?></li>
+            <li>Livro: <?= $livro['titulo'] ?> <br>Autor: <?= $livro['autor'] ?></li>
         <?php endforeach; ?>
     </ul>
 </body>
